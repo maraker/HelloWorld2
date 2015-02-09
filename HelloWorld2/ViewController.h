8ed40762c11686d2da90d09b885e446dba6b8347
Markku Åkerfelt <Markku@Markkus-MacBook-Pro.local>
2015-02-09 16:00:25 +0200

    Initial Commit

---
+++ HelloWorld2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld2
+//
+//  Created by Markku Åkerfelt on 09/02/15.
+//  Copyright (c) 2015 Markku Åkerfelt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
